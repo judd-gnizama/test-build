4362c8d7ea04234aea5a3eddd955bcdb341ec815
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-03-26 01:06:07 +0800

    more

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,6 @@ export default async function Home() {
   return (
     <div className="flex flex-col justify-center">
       <SearchBox/>
-      <p>{results}</p>
       <Results results={results}/>
     
     </div>
