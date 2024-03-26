53e239dc7182ad93d92249b5947da60c04fb436c
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-03-26 23:52:08 +0800

    fixed movie page image

--- src/app/movie/[movieId]/page.jsx
+++ src/app/movie/[movieId]/page.jsx
@@ -1,3 +1,6 @@
+import Ratings from '@/components/Ratings';
+import TagList from '@/components/TagList';
+import Image from 'next/image'
 
 const API_KEY = process.env.API_KEY;
 
@@ -11,9 +14,46 @@ export default async function MoviePage({ params }) {
     const movie = await res.json();
 
   return (
-    <div className="flex flex-col justify-center p-4 mx-auto">
-      <h1 className="text-lg font-bold">Title:  {movie.name || movie.title}</h1>
-      <p>Description:  {movie.overview}</p>
+    <div className="flex flex-col justify-center items-center text-lg gap-4 p-4 mx-auto max-w-6xl">
+      {movie.backdrop_path || movie.poster_path ? 
+        <Image
+        width={500}
+        height={300}
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`}
+        alt="movie picture"
+        style={{
+          width: "100%",
+          height: "100%",
+          // objectFit: "cover",
+        }}
+        ></Image>
+        : 
+        <span 
+          className="material-symbols-outlined bg-slate-400 text-center content-center"
+          style={{
+            width: "100%",
+            aspectRatio: "16/9",
+          }}
+        >hide_image</span>
+        }
+        <article className="flex flex-col gap-4 text-left p-6">
+          <h1 className=" text-4xl font-bold text-amber-300">{movie.name || movie.title}</h1>
+          <p className="">
+            <strong>Description: </strong>
+            <br/>
+            {movie.overview || "No Description Available"}
+          </p>
+          <p>
+            <strong>Rating:</strong>
+            <Ratings/>
+          </p>
+          
+          <p>
+            <strong>Tags:</strong>
+            <br/>
+            <TagList/>
+          </p>
+        </article>
     </div>
   )
 }
---
+++ src/components/Ratings.jsx
@@ -0,0 +1,8 @@
+
+export default function Ratings() {
+  return (
+    <div>
+        Ratings
+    </div>
+  )
+}
