3cf1428f21b8e58deab046971a529fe3f5b8cb0e
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-03-27 10:40:52 +0800

    scale 0.9

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet"/>
-        <meta name="viewport" content="width=device-width"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=0.9"></meta>
 
       </head>
       <body className={inter.className}>
